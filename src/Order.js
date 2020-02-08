e5fa89bf93913b58ac1f0d71d4c61e7ab6cd1c5a
jaca1119 <laki1663@gmail.com>
2020-02-08 18:56:29 +0100

    Title, favicon, empty order, del duck img

--- src/Order.js
+++ src/Order.js
@@ -3,16 +3,26 @@ import React from 'react';
 class Order extends React.Component{
 
     render(){
+
+        let occurances = 0;
+        let details = this.props.location.state.data.map((info) => {
+            if (info.value > 0)
+            {
+                occurances++;
+                return <p key={info.id}>{info.value}x {info.name}</p>;
+            }
+            else
+                return null;
+        });
+
+        if (occurances === 0)
+        {
+            details = <p>Empty order</p>;
+        }
     return (
         <div className="order"> 
            <p>Order details:</p>
-            {this.props.location.state.data.map((info) => {
-                if (info.value > 0)
-                    return <p key={info.id}>{info.value}x {info.name}</p>;
-                else
-                    return null;
-            }
-            )}
+            {details}
             <div>
                 <button onClick={this.props.history.goBack} className="button">Go back</button>
             </div>   
