f5333cdb25bc38c41e2c9b36b4b0260a018d1483
jaca1119 <laki1663@gmail.com>
2020-05-11 17:40:27 +0200

    Changed name of composed pizza

--- src/Pizzas/ComposedPizza.jsx
+++ src/Pizzas/ComposedPizza.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function ComposedPizza(props) {
 
     let ingredients = props.composedPizza.composed_pizza.addonsInput.map(addonInput => addonInput.addon.name + " x " + addonInput.amount).join(", ");
-    
+
     return (
-        <div className="pizza">        
-            <h3>Name: Created pizza</h3>
+        <div className="pizza">
+            <h3>Name: Custom pizza</h3>
             <hr />
             <div className="menu-ingredients">
                 Ingredients: {ingredients}
